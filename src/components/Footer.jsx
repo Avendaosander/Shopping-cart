3856a8bed0e276914f1c0d52b115329b78b36851
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-17 01:22:00 -0400

    Footer Agregado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer () {
+   return (
+      <footer className='fixed left-4 bottom-4 text-left bg-black/70 text-zinc-200 text-sm py-2 px-6 rounded-[2rem] opacity-95 backdrop-blur'>
+         <h4 className='flex'>Prueba técnica de React ⚛️ － <span className='text-blue-700 opacity-80'>@Avendaosander</span></h4>
+         <h5 className='flex'>Shopping Cart con useContext & useReducer</h5>
+      </footer>
+   )
+}
+
+export default Footer
