16f6f1011b04c1f4f8af3f2e3a439d7c015880ff
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-06-17 01:23:21 -0400

    Contexto de carrito agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import { useFilters } from './hooks/useFilters'
+import { CartProvider } from './context/cart'
 import { products as initialProducts } from './mocks/products.json'
 import Header from './components/Header'
+import Cart from './components/Cart'
 import Products from './components/Products'
+import Footer from './components/Footer'
 
 function App() {
    const [products] = useState(initialProducts)
@@ -10,10 +13,12 @@ function App() {
    const filteredProducts = filterProducts(products)
 
    return (
-      <>
+      <CartProvider>
          <Header/>
+         <Cart/>
          <Products products={filteredProducts}/>
-      </>
+         <Footer/>
+      </CartProvider>
    )
 }
 
